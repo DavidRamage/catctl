5df14a2f17cd12d84b2181481e1bbf5982ea147a
David Ramage <david.p.ramage@gmail.com>
2026-03-21 20:55:21 -0700

    set rts and dts from config

--- catfunctions/serial.go
+++ catfunctions/serial.go
@@ -32,7 +32,8 @@ func SendCommand(cnf SerialConf, cmd string) string {
 		log.Fatal(err)
 		os.Exit(-1)
 	}
-	port.SetRTS(false)
+	port.SetRTS(cnf.rts)
+	port.SetDTR(cnf.dtr)
 	if !strings.HasSuffix(cmd, ";") {
 		cmd += ";"
 	}
