6e6a657d504ad7f4a24af14798f52d2fc641aeed
David Ramage <david.p.ramage@gmail.com>
2026-04-18 11:02:54 -0700

    allow formatting of frequency in kilo or mega as well as hz

---
+++ catfunctions/ioFormatting.go
@@ -0,0 +1,29 @@
+package catfunctions
+
+import (
+	"fmt"
+	"strconv"
+	"unicode"
+)
+
+func ToHz(s string) (string, error) {
+	var multiplier int
+	unit := s[len(s)-1]
+	if unicode.IsDigit(rune(unit)) {
+		return s, nil
+	}
+	freq, err := strconv.ParseFloat(s[:len(s)-1], 64)
+	if err != nil {
+		return "0", fmt.Errorf("invalid frequency format: %s", s)
+	}
+	switch unit {
+	case 'k', 'K':
+		multiplier = 1000
+	case 'm', 'M':
+		multiplier = 1000000
+	default:
+		return "0", fmt.Errorf("invalid frequency format: %s", s)
+	}
+	hz := strconv.FormatInt(int64(freq*float64(multiplier)), 10)
+	return hz, nil
+}
