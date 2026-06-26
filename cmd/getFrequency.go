08ab2059336bebf1c86b29c84589a296c8e6a170
David Ramage <david.p.ramage@gmail.com>
2026-06-25 20:41:29 -0700

    use the formatting

--- cmd/getFrequency.go
+++ cmd/getFrequency.go
@@ -39,7 +39,12 @@ to quickly create a Cobra application.`,
 			fmt.Println("Error: ", err)
 			os.Exit(-1)
 		} else {
-			fmt.Println(cmdOut)
+			freqFmt, err := catfunctions.FmtFrequencyOut(cmdOut, "mhz", 2)
+			if err != nil {
+				fmt.Println("Error: ", err)
+				os.Exit(-1)
+			}
+			fmt.Println(freqFmt)
 		}
 	},
 }
